247001f74ae3381fcd3a5213f7a820aaa11e6c0c
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-01-17 10:52:52 +0530

    Created and styled form

---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input type="text" className="input" placeholder="Top text" />
+                <input type="text" className="input" placeholder="Bottom text" />
+                <button className="form-btn">Get a new meme image  🖼</button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme;
