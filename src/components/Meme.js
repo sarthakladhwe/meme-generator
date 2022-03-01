194e9609727ac182b1bef48367fad3877892677a
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-03-01 08:21:55 +0530

    Adding state variable to the formData

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,25 @@
+import React from 'react';
+
 function Meme() {
+
+    const [formData, setFormData] = React.useState({
+        topText: '',
+        bottomText: ''
+    })
+
+    function handleChange() {
+
+    }
+
+    function formSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
         <main>
-            <form className="form">
-                <input type="text" className="input" placeholder="Top text" />
-                <input type="text" className="input" placeholder="Bottom text" />
+            <form className="form" onSubmit={formSubmit}>
+                <input type="text" className="input" placeholder="Top text" onChange={handleChange} value={formData.topText}/>
+                <input type="text" className="input" placeholder="Bottom text" onChange={handleChange} value={formData.bottomText}/>
                 <button className="form-btn">Get a new meme image  🖼</button>
             </form>
         </main>
